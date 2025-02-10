b3819e3be7cb824efe078580fae86ebfb38d6998
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-10 16:53:52 +0800

    feat: sort each half of split arrays Sort each half of split arrays, but not completely solving the project.

--- merge-sort.js
+++ merge-sort.js
@@ -1,7 +1,18 @@
 function mergeSort(arr) {
-    const leftArr = arr.slice(0, Math.floor(arr.length / 2));
-    const rightArr = arr.slice(Math.floor(arr.length / 2));
-    console.log(leftArr, rightArr);
+    if (arr.length === 1) {
+        console.log("Ping")
+        return arr[0]
+    }
+    if (arr.length === 2) {
+        console.log("Ping 2")
+        if (arr[0] > arr[1]) {
+            return [arr[1], arr[0]];
+        } else return [arr[0], arr[1]];
+    }
+    const leftArr = mergeSort(arr.slice(0, Math.floor(arr.length / 2)));
+    const rightArr = mergeSort(arr.slice(Math.floor(arr.length / 2)));
+
+    return leftArr.concat(rightArr);
 }
 
 const test1 = [3, 2, 1, 13, 8, 5, 0, 1]; 
